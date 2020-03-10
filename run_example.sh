3b8704d3f284e27cc7e6afcf1c6bb3000334cad9
lichar yuan <ylc0003@gmail.com>
2020-03-10 11:23:45 +0800

    fix script

--- run_example.sh
+++ run_example.sh
@@ -1,2 +1,3 @@
-# Set Devices   |  distrubuted script | Num of GPUS |  Path of data | 
-CUDA_VISIBLE_DEVICES=2,3 ./distributed_train.sh 2 /data/ImageNet --model DNA_a  --sched cosine --epochs 150 --warmup-epochs 5 --drop 0.2 --lr 0.4 --batch-size 64 -j 8 --num-classes 1000 --model-ema 
+#DNA_a/ DNA_b / DNA_c / DNA_d
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 ./distributed_train.sh 4 /data/ImageNet  --model DNA_*  \
+--epochs 500  --warmup-epochs 5  --batch-size 128 --lr 0.064 --opt rmsproptf --opt-eps 0.001 --sched step --decay-epochs 3 --decay-rate 0.963 --color-jitter 0.06 --drop 0.2  -j 8 --num-classes 1000 --model-ema
